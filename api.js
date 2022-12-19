6f630317adbf4752853999344758b15fe33e0e2f
Vision Rijal <82150577+visionrijal@users.noreply.github.com>
2022-12-19 22:22:55 +0545

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 class Weather{
     
     async getWeather(location){
-        let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=fa1c37346844472d84731725221512&q= ${location}`)
+        let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=fa1c37346844472d84731725221512&q= ${location}`)
         let weatherData = await response.json();
         return weatherData;
     }
